1da2ae3ded382416ad2833b19aec4d8ad140c5d1
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-02-15 13:47:41 -0500

    customising interfaces

--- .jupyter/custom/custom.js
+++ .jupyter/custom/custom.js
@@ -27,12 +27,15 @@ define(function (require, exports, module) {
         NotebookList.prototype.parent_selection_changed = NotebookList.prototype._selection_changed;
         NotebookList.prototype.parent_add_link = NotebookList.prototype.add_link;
         NotebookList.prototype.parent_bind_events = NotebookList.prototype.bind_events;
-        NotebookList.prototype.parent_new_item = NotebookList.prototype.new_item;
 
         NotebookList.prototype.bind_events = function () {
             this.parent_bind_events();
-            // Bind events for action buttons.
-            $('.shared-button').off('click').on('click', $.proxy(this.toggle_shared_selected, this));
+            // Bind events for singleton controls.
+            if (!NotebookList._custom_bound_singletons) {
+                NotebookList._custom_bound_singletons = true;
+                // Bind events for action buttons.
+                $('.shared-button').off('click').on('click', $.proxy(this.toggle_shared_selected, this));
+            }
         };
 
         NotebookList.prototype.toggle_shared_selected = function () {
@@ -55,7 +58,8 @@ define(function (require, exports, module) {
             var that = this,
                 checked = 0,
                 has_read_only = false,
-                has_directories = false;
+                has_directories = false,
+                anonymous = $('.list_item').last().data('path').match(/^-\/-\//);
 
             $('.list_item :checked').each(function (index, item) {
                 var parent = $(item).parent().parent(),
@@ -73,24 +77,38 @@ define(function (require, exports, module) {
             });
 
             // Shared is only visible when one item is selected.
-            if (checked > 0 && !has_read_only) {
+            if (checked > 0 && !anonymous && !has_read_only) {
                 $('.shared-button').css('display', 'inline-block');
             } else {
                 $('.shared-button').css('display', 'none');
             }
 
             // Rename is only visible when one item is selected, it is not a running notebook and writable.
-            if (checked === 1 && !this.is_running(this.selected[0]) && !has_read_only) {
+            if (checked === 1 && !anonymous && !this.is_running(this.selected[0]) && !has_read_only) {
                 $('.rename-button').css('display', 'inline-block');
             } else {
                 $('.rename-button').css('display', 'none');
             }
 
+            // Duplicate isn't visible when a directory is selected.
+            if (checked > 0 && !anonymous && !has_directories) {
+                $('.duplicate-button').css('display', 'inline-block');
+            } else {
+                $('.duplicate-button').css('display', 'none');
+            }
+
             // Delete is visible if one or more items are selected.
-            if (checked > 0 && !has_read_only && !has_directories) {
+            if (checked > 0 && !anonymous && !has_read_only && !has_directories) {
                 $('.delete-button').css('display', 'inline-block');
             } else {
                 $('.delete-button').css('display', 'none');
+            }
+
+            // Delete is visible if one or more items are selected.
+            if (!anonymous) {
+                $('.new-buttons').css('display', 'inline-block');
+            } else {
+                $('.new-buttons').css('display', 'none');
             }
         };
 
