a5595b2e1f14917743a9db5683e9d02923142c93
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-03-06 15:58:53 -0500

    add bash kernel

--- startup.sh
+++ startup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+# Register ruby kernel
 iruby register
+
+# Register nodejs kernel
 ./node_modules/.bin/ijs --ijs-install-kernel
 
+# Register bash kernel
+python -m bash_kernel.install
+
+# Startup jupyter notebook application.
 jupyter notebook --no-browser --no-mathjax --ip=* --port $PORT
 
